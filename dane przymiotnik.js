7789aed47f6e42ae92266353a2fb3f8eccef07bc
Tysek64 <74345334+Tysek64@users.noreply.github.com>
2022-04-16 15:33:28 +0200

    Add files via upload

---
+++ dane przymiotnik.js
@@ -0,0 +1,532 @@
+﻿var dane = [
+    {
+        de: "alt",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "breit",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "dick",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "gewohnt",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "groß",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "hoch",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "los sein",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "tief",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "lang",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "schwer",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "weit",
+        p1: 8,
+        p2: 1
+    },
+    {
+        de: "wert",
+        p1: 10,
+        p2: 1
+    },
+    {
+        de: "ähnlich",
+        p1: 4,
+        p2: 1
+    },
+    {
+        de: "angenehm",
+        p1: 4,
+        p2: 1
+    },
+    {
+        de: "behilflich",
+        p1: 4,
+        p2: 8
+    },
+    {
+        de: "bekannt",
+        p1: 12,
+        p2: 273
+    },
+    {
+        de: "böse",
+        p1: 12,
+        p2: 5
+    },
+    {
+        de: "dankbar",
+        p1: 12,
+        p2: 17
+    },
+    {
+        de: "fremd",
+        p1: 4,
+        p2: 1
+    },
+    {
+        de: "gewachsen sein",
+        p1: 4,
+        p2: 1
+    },
+    {
+        de: "gleich",
+        p1: 4,
+        p2: 1
+    },
+    {
+        de: "lieb",
+        p1: 4,
+        p2: 8193
+    },
+    {
+        de: "möglich",
+        p1: 12,
+        p2: 17
+    },
+    {
+        de: "nahe",
+        p1: 4,
+        p2: 1
+    },
+    {
+        de: "nützlich",
+        p1: 12,
+        p2: 17
+    },
+    {
+        de: "recht",
+        p1: 4,
+        p2: 1
+    },
+    {
+        de: "treu",
+        p1: 4,
+        p2: 1
+    },
+    {
+        de: "überlegen",
+        p1: 4,
+        p2: 1
+    },
+    {
+        de: "willkommen",
+        p1: 4,
+        p2: 1
+    },
+    {
+        de: "bewusst",
+        p1: 2,
+        p2: 1
+    },
+    {
+        de: "gewiss",
+        p1: 2,
+        p2: 1
+    },
+    {
+        de: "sicher",
+        p1: 6,
+        p2: 4097
+    },
+    {
+        de: "schuldig",
+        p1: 2,
+        p2: 1
+    },
+    {
+        de: "verdächtig",
+        p1: 2,
+        p2: 1
+    },
+    {
+        de: "würdig",
+        p1: 2,
+        p2: 1
+    },
+    {
+        de: "arm",
+        p1: 4,
+        p2: 2
+    },
+    {
+        de: "erkrankt",
+        p1: 4,
+        p2: 2
+    },
+    {
+        de: "interessiert",
+        p1: 4,
+        p2: 2
+    },
+    {
+        de: "reich",
+        p1: 4,
+        p2: 2
+    },
+    {
+        de: "schuld",
+        p1: 4,
+        p2: 2
+    },
+    {
+        de: "gebunden",
+        p1: 8,
+        p2: 2
+    },
+    {
+        de: "gewöhnt",
+        p1: 8,
+        p2: 2
+    },
+    {
+        de: "blind",
+        p1: 4,
+        p2: 4
+    },
+    {
+        de: "taub",
+        p1: 4,
+        p2: 4
+    },
+    {
+        de: "angewiesen",
+        p1: 8,
+        p2: 4
+    },
+    {
+        de: "aufmerksam",
+        p1: 8,
+        p2: 4
+    },
+    {
+        de: "eifersüchtig",
+        p1: 8,
+        p2: 4
+    },
+    {
+        de: "gespannt",
+        p1: 8,
+        p2: 4
+    },
+    {
+        de: "neugierig",
+        p1: 8,
+        p2: 4
+    },
+    {
+        de: "neidisch",
+        p1: 8,
+        p2: 4
+    },
+    {
+        de: "stolz",
+        p1: 8,
+        p2: 4
+    },
+    {
+        de: "wütend",
+        p1: 8,
+        p2: 4
+    },
+    {
+        de: "beliebt",
+        p1: 4,
+        p2: 8
+    },
+    {
+        de: "verhasst",
+        p1: 4,
+        p2: 8
+    },
+    {
+        de: "charakteristisch",
+        p1: 8,
+        p2: 16
+    },
+    {
+        de: "geeignet",
+        p1: 8,
+        p2: 16
+    },
+    {
+        de: "interessant",
+        p1: 8,
+        p2: 16
+    },
+    {
+        de: "klar",
+        p1: 8,
+        p2: 16
+    },
+    {
+        de: "notwendig",
+        p1: 8,
+        p2: 16
+    },
+    {
+        de: "schädlich",
+        p1: 8,
+        p2: 16
+    },
+    {
+        de: "verständlich",
+        p1: 8,
+        p2: 16
+    },
+    {
+        de: "vorteilhaft",
+        p1: 8,
+        p2: 16
+    },
+    {
+        de: "wichtig",
+        p1: 8,
+        p2: 16
+    },
+    {
+        de: "zuständig",
+        p1: 8,
+        p2: 16
+    },
+    {
+        de: "allergisch",
+        p1: 8,
+        p2: 32
+    },
+    {
+        de: "empfindlich",
+        p1: 8,
+        p2: 32
+    },
+    {
+        de: "gleichgültig",
+        p1: 8,
+        p2: 32
+    },
+    {
+        de: "misstrauisch",
+        p1: 8,
+        p2: 32
+    },
+    {
+        de: "streng",
+        p1: 8,
+        p2: 32
+    },
+    {
+        de: "verliebt",
+        p1: 8,
+        p2: 64
+    },
+    {
+        de: "bewandert",
+        p1: 4,
+        p2: 64
+    },
+    {
+        de: "erfahren",
+        p1: 4,
+        p2: 64
+    },
+    {
+        de: "geübt",
+        p1: 4,
+        p2: 64
+    },
+    {
+        de: "nachlässig",
+        p1: 4,
+        p2: 64
+    },
+    {
+        de: "wohnhaft",
+        p1: 4,
+        p2: 64
+    },
+    {
+        de: "befreundet",
+        p1: 4,
+        p2: 128
+    },
+    {
+        de: "einverstanden",
+        p1: 4,
+        p2: 128
+    },
+    {
+        de: "fertig",
+        p1: 4,
+        p2: 128
+    },
+    {
+        de: "vergleichbar",
+        p1: 4,
+        p2: 128
+    },
+    {
+        de: "verheiratet",
+        p1: 4,
+        p2: 128
+    },
+    {
+        de: "versehen",
+        p1: 4,
+        p2: 128
+    },
+    {
+        de: "zufrieden",
+        p1: 4,
+        p2: 128
+    },
+    {
+        de: "ehrgeizig",
+        p1: 4,
+        p2: 256
+    },
+    {
+        de: "gierig",
+        p1: 4,
+        p2: 256
+    },
+    {
+        de: "verrückt",
+        p1: 4,
+        p2: 256
+    },
+    {
+        de: "ärgerlich",
+        p1: 8,
+        p2: 512
+    },
+    {
+        de: "entsetzt",
+        p1: 8,
+        p2: 512
+    },
+    {
+        de: "erfreut",
+        p1: 8,
+        p2: 512
+    },
+    {
+        de: "erstaunt",
+        p1: 8,
+        p2: 512
+    },
+    {
+        de: "froh",
+        p1: 8,
+        p2: 512
+    },
+    {
+        de: "glücklich",
+        p1: 8,
+        p2: 512
+    },
+    {
+        de: "traurig",
+        p1: 8,
+        p2: 512
+    },
+    {
+        de: "besorgt",
+        p1: 8,
+        p2: 1024
+    },
+    {
+        de: "abhängig",
+        p1: 4,
+        p2: 2048
+    },
+    {
+        de: "beeindruckt",
+        p1: 4,
+        p2: 2048
+    },
+    {
+        de: "begeistert",
+        p1: 4,
+        p2: 2048
+    },
+    {
+        de: "frei",
+        p1: 4,
+        p2: 2048
+    },
+    {
+        de: "voll",
+        p1: 4,
+        p2: 2048
+    },
+    {
+        de: "blass",
+        p1: 4,
+        p2: 4096
+    },
+    {
+        de: "rot",
+        p1: 4,
+        p2: 4096
+    },
+    {
+        de: "stumm",
+        p1: 4,
+        p2: 4096
+    },
+    {
+        de: "bereit",
+        p1: 4,
+        p2: 8192
+    },
+    {
+        de: "fähig",
+        p1: 4,
+        p2: 8192
+    },
+    {
+        de: "entschlossen",
+        p1: 4,
+        p2: 8192
+    },
+    {
+        de: "freundlich",
+        p1: 4,
+        p2: 8192
+    },
+    {
+        de: "nett",
+        p1: 4,
+        p2: 8192
+    }
+];
